1033ae26ffe0fb07b856abcac4a4b456dca42b72
Washington Fidelix <fidelix.silva25@gmail.com>
2025-03-25 11:52:48 -0300

    Aula02 - Exercícios

---
+++ Aula02/00_exercicios.py
@@ -0,0 +1,18 @@
+# nomeAluno = "Gustavo"
+
+# print(type(nomeAluno))
+
+# print(nomeAluno.lower())
+# print(nomeAluno.upper())
+
+# Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+valorUm = int(input("insira um número inteiro: "))
+valorDois = int(input("insira um número inteiro: "))
+valorTres = valorUm + valorDois
+print(valorTres)
+
+
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+
+# Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
