1078d106114b47994ccb7b1897e98decb6345b22
Washington Fidelix <fidelix.silva25@gmail.com>
2025-03-26 08:41:32 -0300

    Aula + Exercícios finais

---
+++ Aula02/21_exercicios.py
@@ -0,0 +1,7 @@
+# Conversor de Temperatura - Try Except
+try:
+    celsius = float(input("Digite a temperatura em Celsius: "))
+    fahrenheit = (celsius * 9 / 5) + 32
+    print(f"{celsius}ºC é igual a {fahrenheit}ºF.")
+except ValueError:
+    print("Por favor, digite um número válido para a temperatura")
